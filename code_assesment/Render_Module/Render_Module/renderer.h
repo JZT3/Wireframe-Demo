e648f83fbb55601394de62e284cf1480db02cdeb
Jervani Z Thompson <jzthompson20@gmail.com>
2025-03-26 18:38:33 -0700

    Reorganized project to be a static library.

---
+++ code_assesment/Render_Module/Render_Module/renderer.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <memory>
+#include "Vector2D.h"
+#include "Vector3D.h"
+#include "Framebuffer.h"
---
+++ code_assesment/Render_Module/Render_Module/renderer_swap.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+#include <memory>
+#include "vector2d.h"
